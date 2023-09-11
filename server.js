8be580a94ebfc76f3b827fcdf056c074c2a85401
basiacarvalho <basiacarvalho@gmail.com>
2023-09-11 20:15:37 +0200

    initializing mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const uriDb =
+  "mongodb+srv://basiacarvalho:CPEODGZl2A3FR1cm@cluster0.zv2z0gw.mongodb.net/?retryWrites=true&w=majority";
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((err) =>
+    console.log(`Server not running. Error message: ${err.message}`)
+  );
